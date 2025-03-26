1a9335c8b2a1f4a399d2f5ddc60e70df107497d0
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-03-25 21:51:34 -0300

    adiciona tela de cadastro de conta

--- src/screens/CreateAccount/index.tsx
+++ src/screens/CreateAccount/index.tsx
@@ -1,48 +1,115 @@
+import React, { useState } from "react";
 import {
   Text,
   TextInput,
   TouchableOpacity,
   View,
   StyleSheet,
+  Alert,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default function CreateAccount() {
+export default function CreateAccount({ navigation }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleCreateAccount = () => {
+    if (!name) {
+      Alert.alert("Aviso", "Preencha o campo nome completo");
+    } else if (!email) {
+      Alert.alert("Aviso", "Preencha o campo email");
+    } else if (!/^\d{3}\.\d{3}\.\d{3}-\d{2}$/.test(cpf)) {
+      Alert.alert(
+        "Aviso",
+        "Preencha o campo CPF no formato válido. Ex: 000.000.000-00"
+      );
+    } else if (password !== confirmPassword) {
+      Alert.alert("Aviso", "As senhas não coincidem");
+    } else {
+      Alert.alert("Sucesso", "Conta criada com sucesso!");
+      navigation.navigate("Login");
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ScrollView>
+          <Text style={styles.createAccountText}>Criar conta</Text>
+          <Text style={styles.descriptionText}>
+            Crie uma conta agora e ganhe seu cartão de crédito com R$ 1000,00 de
+            limite!
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome completo"
+            cursorColor="#150230"
+            autoCapitalize="words"
+            value={name}
+            onChangeText={setName}
+            autoFocus
+          />
 
-      <TextInput
-        style={styles.input}
-        placeholder="CPF"
-        cursorColor="#150230"
-        keyboardType="number-pad"
-      />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            cursorColor="#150230"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={setEmail}
+          />
 
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        cursorColor="#150230"
-        secureTextEntry
-      />
+          <TextInput
+            style={styles.input}
+            placeholder="CPF"
+            cursorColor="#150230"
+            keyboardType="number-pad"
+            value={cpf}
+            onChangeText={setCpf}
+          />
 
-      <TouchableOpacity>
-        <Text style={styles.linkText}>Esqueceu a senha ?</Text>
-      </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            cursorColor="#150230"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
 
-      <TouchableOpacity style={styles.buttonLogin}>
-        <Text>Entrar</Text>
-      </TouchableOpacity>
+          <TextInput
+            style={styles.input}
+            placeholder="Confirmar senha"
+            cursorColor="#150230"
+            secureTextEntry
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          <TouchableOpacity
+            onPress={handleCreateAccount}
+            style={styles.buttonLogin}
+          >
+            <Text>Criar conta</Text>
+          </TouchableOpacity>
 
-      <TouchableOpacity>
-        <Text style={[styles.linkText, { textAlign: "center" }]}>
-          Criar conta
-        </Text>
-      </TouchableOpacity>
-    </View>
+          <TouchableOpacity>
+            <Text style={[styles.linkText, { textAlign: "center" }]}>
+              Já tem uma conta? Faça login
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-    
   container: {
     flex: 1,
     backgroundColor: "#150230",
@@ -70,5 +137,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: 44,
     marginBottom: 20,
+  },
+  createAccountText: {
+    color: "#FFF",
+    fontSize: 34,
+    marginBottom: 10,
+  },
+  descriptionText: {
+    color: "#FFF",
+    fontSize: 16,
+    marginBottom: 40,
   },
 });
